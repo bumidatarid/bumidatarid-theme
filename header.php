8688092f9cdf1204c019b88ea7c2151a26212ec2
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2018-04-29 05:26:16 +0000

    set up flatearthws app id

--- header.php
+++ header.php
@@ -39,7 +39,7 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=644901275852993&autoLogAppEvents=1';
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=1834575003272531&autoLogAppEvents=1';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 <?php endif ?>
