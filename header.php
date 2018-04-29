77fe462fb4f7c9e09ad588a770e07e9132c75c9c
Priyadi Iman Nurcahyo <priyadi@priyadi.net>
2018-04-29 06:11:53 +0000

    twitter widget.js

--- header.php
+++ header.php
@@ -44,6 +44,22 @@
 }(document, 'script', 'facebook-jssdk'));</script>
 <?php endif; ?>
 
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));</script>
 
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
